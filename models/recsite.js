900d8cfbd51565f41a58fd4db6bd1664da2d45e5
Scott Gerweck <scottgerweck@gmail.com>
2021-10-30 01:15:56 -0700

    added data seed files, schemas

---
+++ models/recsite.js
@@ -0,0 +1,26 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const recsiteSchema = new Schema({
+    name: {
+        type: String,
+        required: true
+    },
+    location: {
+        type: String,
+        required: true
+    },
+    type: {
+        type: String,
+        default: false
+    },
+    description: {
+        type: String,
+        required: true
+    }
+},
+{
+    timestamps: true
+});
+
+module.exports = mongoose.model('Recsite', recsiteSchema);
