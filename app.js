ef97f31fe26b809c80fdbba7c088c6d6609e0db9
Scott Alan <scottalandev@gmail.com>
2022-01-05 15:30:02 -0800

    Refactored and ready for initial deploy. Commented out (but did not delete) deprecated https express methods. Instead will use nginx/certbot to handle/secure traffic. Refactored (and built) react client with static images/vid integrated (no longer requires api call). Created and ironed out /api routing. Numerous other tweaks.

--- app.js
+++ app.js
@@ -29,14 +29,14 @@ connect.then(() => console.log('Connected correctly to server'),
 var app = express();
 
 // Secure traffic only
-app.all('*', (req, res, next) => {
-    if (req.secure) {
-        return next();
-    } else {
-        console.log(`Redirecting to: https://${req.hostname}:${app.get('secPort')}${req.url}`);
-        res.redirect(301, `https://${req.hostname}:${app.get('secPort')}${req.url}`);
-    }
-});
+// app.all('*', (req, res, next) => {
+//     if (req.secure) {
+//         return next();
+//     } else {
+//         console.log(`Redirecting to: https://${req.hostname}:${app.get('secPort')}${req.url}`);
+//         res.redirect(301, `https://${req.hostname}:${app.get('secPort')}${req.url}`);
+//     }
+// });
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -47,17 +47,20 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
 //default Router inclusions
-app.use('/', indexRouter);
+app.use('/api', indexRouter);
 app.use('/users', usersRouter);
 
 //my Router inclusions
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, 'react-build')));
+app.use(express.static(path.join(__dirname, 'react-build', '*')));
 
-app.use('/recsites', recSiteRouter);
-app.use('/routes', routeRouter);
-app.use('/tripreports', tripReportRouter);
-app.use('/userroutes', userRouteRouter);
-app.use('/userrecsites', userRecSiteRouter);
+
+app.use('/api/recsites', recSiteRouter);
+app.use('/api/routes', routeRouter);
+app.use('/api/tripreports', tripReportRouter);
+app.use('/api/userroutes', userRouteRouter);
+app.use('/api/userrecsites', userRecSiteRouter);
 
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
